4e656525d01dd961d4661db8406c528de4f3ceb6
Daniel.Ismael <daniel.ismael@encoparts.com>
2024-02-20 10:19:37 -0300

    feat: add button excel

--- src/components/Listings/quotation/listing.tsx
+++ src/components/Listings/quotation/listing.tsx
@@ -3,6 +3,9 @@ import Image from "next/image";
 import DataTable from "@/src/components/Datatable";
 import LinkSmall from "@/src/components/LinkSmall";
 import {useRouter} from "next/router";
+import {getApiDealer} from "@/src/api/dealer/axios";
+import {useMessageError} from "@/src/hooks/message/error";
+import {useMessageSuccess} from "@/src/hooks/message/success";
 
 interface QuotationProps {
     quotation: {}
@@ -11,8 +14,49 @@ interface QuotationProps {
 const Listing = ({ quotation } : QuotationProps) => {
     const router = useRouter()
 
+    const { setMessageError } = useMessageError()
+    const { showMessage } = useMessageSuccess()
+
     const handleToViewQuotation = (id: number) => {
         router.push(`/quotation/${id}`)
+    }
+
+    const handleRequestExcel = async (id: number) => {
+        const api = getApiDealer('')
+        await api.post('/quotation/generate-excel', {
+            id_quotation: id
+        })
+            .then((response) => {
+                const fileUrl = response?.data?.data?.original?.url;
+
+                if (fileUrl) {
+                    // Concatenar a URL base com a URL do arquivo
+                    //const fullUrl = 'http://127.0.0.1:9000'+fileUrl;
+                    const fullUrl = 'https://b2b-qas.azurewebsites.net'+fileUrl;
+                    const link = document.createElement('a');
+                    link.href = fullUrl;
+                    link.download = 'quotation.xlsx';
+                    document.body.appendChild(link);
+                    link.click();
+                    document.body.removeChild(link);
+
+                    showMessage('Your Excel file download is ready.')
+                } else {
+                    setMessageError('Error while fetching file URL.');
+                }
+            })
+            .catch((e) => {
+                console.log(e);
+                let errorString = ""
+
+                Object.keys(e?.response?.data?.errors).forEach((key) => {
+                    e?.response?.data?.errors[key].forEach((errorMessage: any) => {
+                        errorString += `${errorMessage}<br>`
+                    })
+                })
+
+                setMessageError(errorString)
+            })
     }
 
     const columns = [
@@ -61,11 +105,22 @@ const Listing = ({ quotation } : QuotationProps) => {
             accessor: "action",
             width: "5%",
             Cell: ({ row }: any) => (
-                <div className="flex items-center w-auto text-right">
-                    <button onClick={() => handleToViewQuotation(row.original.id)} className="w-25px h-25px flex items-center justify-centers">
-                        <Image src="/icon/icon-view.svg" width="18" height="18" alt="icon edit" />
-                    </button>
-                </div>
+                <>
+                    <div className="flex items-center w-auto text-right">
+                        <button onClick={() => handleToViewQuotation(row.original.id)}
+                                className="w-25px h-25px flex items-center justify-centers">
+                            <Image src="/icon/icon-view.svg" width="18" height="18" alt="icon edit"/>
+                        </button>
+                    </div>
+                    {row.original.status == "Conclude" && (
+                        <div className="flex items-center w-auto text-right">
+                            <button onClick={() => handleRequestExcel(row.original.id)}
+                                    className="w-25px h-25px flex items-center justify-centers">
+                                <Image src="/icon/icon-download-excel.svg" width="18" height="18" alt="icon excel"/>
+                            </button>
+                        </div>
+                    )}
+                </>
             ),
         }
     ]
@@ -75,7 +130,7 @@ const Listing = ({ quotation } : QuotationProps) => {
             columns={columns}
             data={quotation}
             page_size={10}
-            buttons={<LinkSmall bgColor="bg-yellow_one" href="/quotation/new" name="Submit quotation" />}
+            buttons={<LinkSmall bgColor="bg-yellow_one" href="/quotation/new" name="Submit quotation"/>}
         />
     )
 }
